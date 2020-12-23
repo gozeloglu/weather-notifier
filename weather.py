9372101aaa488a5c22b4c831939726e8955c3e6d
gozeloglu <gozeloglu@gmail.com>
2020-12-24 01:35:15 +0300

    comment lines added

--- weather.py
+++ weather.py
@@ -1,4 +1,3 @@
-import sys
 import os
 import requests
 from win10toast import ToastNotifier
@@ -6,6 +5,9 @@ from datetime import datetime
 from enum import Enum
 
 
+# These variables are used in API url
+# You need to have OpenWeather API Key
+# You can register and receive a key from https://openweathermap.org/
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 CITY = os.getenv("CITY")
 
@@ -27,6 +29,18 @@ class Weather(Enum):
     
 
 def get_current_time_slot(sunrise, sunset, timezone):
+    """
+    :param sunrise: sunrise time in UNIX(Epoch) time
+    :type int
+
+    :param sunset: sunset time in UNIX(Epoch) time
+    :type int
+
+    :param timezone: users timezone in UNIX(Epoch) time
+    :type int
+
+    :return: Emoji string wrt time slot 
+    """
     now = datetime.now().strftime("%H:%M")
     sunrise = datetime.fromtimestamp(sunrise).strftime("%A, %B %d, %Y %H:%M:%S")
     sunset = datetime.fromtimestamp(sunset).strftime("%A, %B %d, %Y %H:%M:%S")
@@ -43,7 +57,14 @@ def get_current_time_slot(sunrise, sunset, timezone):
     else:
         return TimeSlot.EVENING.value
     
+
 def get_weather_emoji(weather_id):
+    """
+    Finds the suitable emoji for given weather situation
+    :param weather_id: id of the weather status
+    :type int
+    :return: Emoji string wrt weather status
+    """
     
     if weather_id < 300:
         return Weather.LIGHTHING.value
@@ -62,20 +83,46 @@ def get_weather_emoji(weather_id):
     return Weather.SUNNY.value
 
 def send_notification(weather_emoji, time_slot_emoji, weather_description, tempature):
+    """
+    Send notification with given information
+    
+    :param weather_emoji: current weather's status emoji string
+    :type Weather(enum)
+
+    :param time_slot: suitable emoji string wrt current time's slot.
+    :type TimeSlot(enum)
+
+    :param weather_description: current weather description
+    :type string
+
+    :param tempature: degree of the weather in celsius
+    :type string
+
+    :return: Nothing
+    """
+
     now = datetime.now().strftime("%H:%M")
-    toaster = ToastNotifier()
     city = "City: " + CITY + "\n"
     time = "Time: " + now + time_slot_emoji + "\n"
     description = "Description: " + weather_description + "  " + weather_emoji + "\n"
     temp = "Tempature: " + tempature + "°C " + Weather.THERMOMETER.value
     message = city + time + description + temp 
+    toaster = ToastNotifier()
     toaster.show_toast("Weather Forecast", message,
                    icon_path=None,
                    duration=10,
                    threaded=True)
 
 def parse_json(data):
-    
+    """
+    Parses the data and retrives the necessary information
+
+    :param data: json file which is fetched
+    :type dict
+
+    :return: Nothing
+    """
+
     weather = data["weather"][0]["description"]
     weather_id = int(data["weather"][0]["id"])
     tempature = str(data["main"]["temp"])
@@ -88,11 +135,11 @@ def parse_json(data):
     send_notification(emoji, time_slot, weather, tempature)
 
 
-def get_weather_information():
+def main():
     url = "http://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&APPID=" + WEATHER_API_KEY + "&units=metric"
     res = requests.get(url)
     data = res.json()
     parse_json(data)
 
 
-get_weather_information()
+main()
